c50dc7b6cd58c698e8af51dfb00793148c7f4844
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-06-19 22:30:24 +0800

    Add static products

---
+++ data/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: "1",
+    productName: "Turon",
+    description: "Crispy caramelized banana rolls",
+    price: 25,
+  },
+  {
+    id: "2",
+    productName: "Karioka",
+    description: "Chewy, sweet deep-fried rice balls",
+    price: 15,
+  },
+  {
+    id: "3",
+    productName: "Maruya",
+    description: "Golden fried banana fritters",
+    price: 12,
+  },
+  {
+    id: "4",
+    productName: "Banana Cue",
+    description: "Skewered sugar-glazed fried bananas",
+    price: 20,
+  },
+];
